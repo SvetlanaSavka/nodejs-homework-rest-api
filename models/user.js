6dd6f467802aa91ea5b761c6f307f6af9154d703
SvetlanaSavka <savkasa@ukr.net>
2022-10-22 22:14:47 +0300

    added Schemaregister subscription, token:null, fixed test condition

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const Joi = require("joi");
 const handleSaveErrors = require("../helpers/handleSaveErrors");
 
 const emailRegexp = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+const subscriptions = ["starter", "pro", "business"];
 
 const userSchema = new Schema(
   {
@@ -20,12 +21,12 @@ const userSchema = new Schema(
     },
     subscription: {
       type: String,
-      enum: ["starter", "pro", "business"],
+      enum: subscriptions,
       default: "starter",
     },
     token: {
       type: String,
-      default: "",
+      default: null,
     },
   },
   { versionKey: false, timestamps: true }
@@ -35,6 +36,7 @@ userSchema.post("save", handleSaveErrors);
 const registerSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
+  subscription: Joi.string().valid(...subscriptions),
 });
 
 const loginSchema = Joi.object({
